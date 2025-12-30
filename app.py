602cb5f2e7bd0489b067105f46ef94250fd62a6f
William Lu <32563160+williaml12@users.noreply.github.com>
2025-12-30 10:11:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ st.markdown("---")
 # -----------------------
 # Centered Analyze Button
 # -----------------------
-col1, col2, col3 = st.columns([1, 1, 1])
+col1, col2, col3 = st.columns([1, 2, 1])
 
 with col2:
     # analyze_clicked = st.button("🔍 Analyze My Resume", use_container_width=True)
